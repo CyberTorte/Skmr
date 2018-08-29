03c895b00d705466ce1cd6c7c4fd72e970854c26
CyberTorte <kbc16a07@stu.kawahara.ac.jp>
2018-08-30 01:51:37 +0900

    プロトタイプ作成

--- birthday/views.py
+++ birthday/views.py
@@ -5,13 +5,35 @@ from .models import Photo
 
 # Create your views here.
 
-class NatsumiIndexView(generic.ListView):
-    template_name = ''
+class NatsumiBirthdayView(generic.ListView):
+    template_name = 'Natsumi.html'
     context_object_name = 'photo_list'
 
     def get_queryset(self):
         # データを再編成しテンプレートに返す
-        photo_list = Photo.objects.all().order_by('id')
+        photo_list = Photo.objects.filter(person='Natsumi').order_by('id')
+        photo_list = sort_photo(photo_list)
+        return photo_list
 
 def sort_photo(photo_list):
-    for 
+    top_photo_list = []
+    message_photo_list = []
+    sponsor_photo_list = []
+    sorted_photo_list = []
+
+    for photo in photo_list:
+    
+        if photo.kind == 'top':
+            top_photo_list.append(photo)
+        
+        elif photo.kind == 'message':
+            message_photo_list.append(photo)
+
+        elif photo.kind == 'sponsor':
+            sponsor_photo_list.append(photo)
+
+    sorted_photo_list = sorted_photo_list.extend(top_photo_list)
+    sorted_photo_list = sorted_photo_list.extend(message_photo_list)
+    sorted_photo_list = sorted_photo_list.extend(sponsor_photo_list)
+
+    return sorted_photo_list
