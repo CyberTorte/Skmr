41c3e59969ba15b769335ffd49304b1d0d17dd9d
CyberTorte <kbc16a07@stu.kawahara.ac.jp>
2018-09-15 22:39:40 +0900

    [update]トップレベル参照時にinfomationページを表示するよう設定

--- Portal/urls.py
+++ Portal/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from . import views
 
 app_name = 'Portal'
 urlpatterns = [
+    path('', RedirectView.as_view(url='infomation/')),
     path('infomation/', views.infomation, name='infomation'),
 ]
