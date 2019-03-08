94d79e8e0cadd7804519c0f5d88c867f8d647ae5
CyberTorte <endless.story.ms@gmail.com>
2019-03-08 14:28:09 +0900

    [update] ver0.7.26:ジャンボトロンの調整とサムネイル表示

---
+++ Albam/static/Albam/js/detail_jquery.js
@@ -0,0 +1,10 @@
+$(function() {
+    var thunnail_photo = $('.photos .card:first-child img');
+    $('.jumbotron').css({
+        'background': 'linear-gradient(rgba(255, 255, 255, 0.6), rgba(255, 255, 255, 0.6)), url("' + thunnail_photo.attr('src') + '")',
+        'background-size': 'contain',
+        'background-repeat': 'no-repeat',
+        'background-position': 'center 60%',
+        'background-color': 'white'
+    });
+})
