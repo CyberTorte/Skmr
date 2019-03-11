13723932c55e6c261cc43723b63c70aa9af2315b
CyberTorte <endless.story.ms@gmail.com>
2019-03-11 14:25:51 +0900

    [update] ver0.7.33:個別アルバムのモーダルタイトル、ボタン位置調整　画像にカバーをかけた

--- Albam/static/Albam/js/detail_jquery.js
+++ Albam/static/Albam/js/detail_jquery.js
@@ -1,6 +1,8 @@
 $(function() {
-    var thunnail_photo = $('.photos .card:first-child img');
-    $('.jumbotron').css({
+    var jumbotron = $('.jumbotron')
+    var thunnail_photo = $('.photos > .card:first-child > img');
+
+    jumbotron.css({
         'background': 'linear-gradient(rgba(255, 255, 255, 0.6), rgba(255, 255, 255, 0.6)), url("' + thunnail_photo.attr('src') + '")',
         'background-size': 'contain',
         'background-repeat': 'no-repeat',
